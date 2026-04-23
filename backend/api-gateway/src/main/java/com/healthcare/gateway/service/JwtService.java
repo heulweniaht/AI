de605979316ccd8aaf76fabfa50a0ecf976cf9ec
Thai Nguyen <thaiinguyen2003@gmail.com>
2026-04-23 21:16:28 +0700

    clean project

---
+++ backend/api-gateway/src/main/java/com/healthcare/gateway/service/JwtService.java
@@ -0,0 +1,31 @@
+package com.healthcare.gateway.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.security.PublicKey;
+
+@Service
+public class JwtService {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public Claims extraAllClaims(String token) throws JwtException{
+        return Jwts.parser()
+                .verifyWith(getSignKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
